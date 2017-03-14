835728daf4034c387f79b57b23e87a2751421229
LiuMaoLinGit <951442843@qq.com>
2017-03-14 15:44:09 +0800

    bug 修改

--- myLib/src/main/java/lib/adapter/MBaseAdapter.java
+++ myLib/src/main/java/lib/adapter/MBaseAdapter.java
@@ -9,60 +9,63 @@ import android.widget.BaseAdapter;
 import java.util.List;
 
 /**
- * 
  * @author liuml
- *
  */
 public abstract class MBaseAdapter extends BaseAdapter {
-	public LayoutInflater inflater;
-	public Context context;
-	public List<?> list;
+    public LayoutInflater inflater;
+    public Context context;
+    public List<?> list;
 
-	public MBaseAdapter(Context context, List<?> list) {
-		this.context = context;
-		this.list = list;
-		this.inflater = LayoutInflater.from(context);
-	}
-	public void setList(List<?> list){
-		this.list = list;
-		this.notifyDataSetChanged();
-	}
+    public MBaseAdapter(Context context, List<?> list) {
+        this.context = context;
+        this.list = list;
+        this.inflater = LayoutInflater.from(context);
+    }
 
-	public MBaseAdapter(LayoutInflater inflater, List<?> list) {
-		this.context = inflater.getContext();
-		this.list = list;
-		this.inflater = inflater;
-	}
+    public void setList(List<?> list) {
+        this.list = list;
+        this.notifyDataSetChanged();
+    }
 
-	@Override
-	public int getCount() {
-		return list.size();
-	}
+    public MBaseAdapter(LayoutInflater inflater, List<?> list) {
+        this.context = inflater.getContext();
+        this.list = list;
+        this.inflater = inflater;
+    }
 
-	@Override
-	public Object getItem(int position) {
-		return list.get(position);
-	}
+    @Override
+    public int getCount() {
+        if (list != null && list.size() > 0) {
+            return list.size();
+        } else {
+            return 0;
+        }
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		return baseGetView(position, convertView, parent);
-	}
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 
-	/**
-	 * 与BaseAdapter 中的getView() 一样
-	 * 
-	 * @param position
-	 * @param convertView
-	 * @param parent
-	 * @return
-	 */
-	public abstract View baseGetView(int position, View convertView,
-			ViewGroup parent);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return baseGetView(position, convertView, parent);
+    }
+
+    /**
+     * 与BaseAdapter 中的getView() 一样
+     *
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
+    public abstract View baseGetView(int position, View convertView,
+                                     ViewGroup parent);
 
 }
