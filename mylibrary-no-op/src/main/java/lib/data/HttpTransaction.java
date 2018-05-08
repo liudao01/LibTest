d76340f935419d5f6a9701f69f3bc7fc90f463d7
Liuml <951442843@qq.com>
2018-05-08 20:53:43 +0800

    添加其他方法

--- mylibrary-no-op/src/main/java/lib/data/HttpTransaction.java
+++ mylibrary-no-op/src/main/java/lib/data/HttpTransaction.java
@@ -1,5 +1,7 @@
 package lib.data;
 
+import java.util.Date;
+
 /**
  * @author liuml
  * @explain
@@ -7,4 +9,216 @@ package lib.data;
  */
 
 public class HttpTransaction {
+
+    private Long _id;
+    private Date requestDate;
+    private Date responseDate;
+    private Long tookMs;
+
+    private String protocol;
+    private String method;
+    private String url;
+    private String host;
+    private String path;
+    private String scheme = "http";
+
+    private Long requestContentLength;
+    private String requestContentType;
+    private String requestHeaders;
+    private String requestBody;
+    private boolean requestBodyIsPlainText = true;
+
+    private Integer responseCode;
+    private String responseMessage;
+    private String error;
+
+    private Long responseContentLength;
+    private String responseContentType;
+    private String responseHeaders;
+    private String responseBody;
+    private boolean responseBodyIsPlainText = true;
+
+    public Long get_id() {
+        return _id;
+    }
+
+    public void set_id(Long _id) {
+        this._id = _id;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResponseDate() {
+        return responseDate;
+    }
+
+    public void setResponseDate(Date responseDate) {
+        this.responseDate = responseDate;
+    }
+
+    public Long getTookMs() {
+        return tookMs;
+    }
+
+    public void setTookMs(Long tookMs) {
+        this.tookMs = tookMs;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getScheme() {
+        return scheme;
+    }
+
+    public void setScheme(String scheme) {
+        this.scheme = scheme;
+    }
+
+    public Long getRequestContentLength() {
+        return requestContentLength;
+    }
+
+    public void setRequestContentLength(Long requestContentLength) {
+        this.requestContentLength = requestContentLength;
+    }
+
+    public String getRequestContentType() {
+        return requestContentType;
+    }
+
+    public void setRequestContentType(String requestContentType) {
+        this.requestContentType = requestContentType;
+    }
+
+    public String getRequestHeaders() {
+        return requestHeaders;
+    }
+
+    public void setRequestHeaders(String requestHeaders) {
+        this.requestHeaders = requestHeaders;
+    }
+
+    public String getRequestBody() {
+        return requestBody;
+    }
+
+    public void setRequestBody(String requestBody) {
+        this.requestBody = requestBody;
+    }
+
+    public boolean isRequestBodyIsPlainText() {
+        return requestBodyIsPlainText;
+    }
+
+    public void setRequestBodyIsPlainText(boolean requestBodyIsPlainText) {
+        this.requestBodyIsPlainText = requestBodyIsPlainText;
+    }
+
+    public Integer getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(Integer responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Long getResponseContentLength() {
+        return responseContentLength;
+    }
+
+    public void setResponseContentLength(Long responseContentLength) {
+        this.responseContentLength = responseContentLength;
+    }
+
+    public String getResponseContentType() {
+        return responseContentType;
+    }
+
+    public void setResponseContentType(String responseContentType) {
+        this.responseContentType = responseContentType;
+    }
+
+    public String getResponseHeaders() {
+        return responseHeaders;
+    }
+
+    public void setResponseHeaders(String responseHeaders) {
+        this.responseHeaders = responseHeaders;
+    }
+
+    public String getResponseBody() {
+        return responseBody;
+    }
+
+    public void setResponseBody(String responseBody) {
+        this.responseBody = responseBody;
+    }
+
+    public boolean isResponseBodyIsPlainText() {
+        return responseBodyIsPlainText;
+    }
+
+    public void setResponseBodyIsPlainText(boolean responseBodyIsPlainText) {
+        this.responseBodyIsPlainText = responseBodyIsPlainText;
+    }
 }
