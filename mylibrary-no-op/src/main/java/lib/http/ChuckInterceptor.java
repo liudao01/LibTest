c0e3ad0471624f52ab19236b70552daf4343e8cb
Liuml <951442843@qq.com>
2018-05-09 10:36:55 +0800

    test2

--- mylibrary-no-op/src/main/java/lib/http/ChuckInterceptor.java
+++ mylibrary-no-op/src/main/java/lib/http/ChuckInterceptor.java
@@ -2,15 +2,25 @@ package lib.http;
 
 import android.content.Context;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
 /**
  * @author liuml
  * @explain
  * @time 2018/5/8 20:45
  */
 
-public class ChuckInterceptor {
+public final class ChuckInterceptor implements Interceptor {
 
     public ChuckInterceptor(Context context) {
 
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
     }
 }
